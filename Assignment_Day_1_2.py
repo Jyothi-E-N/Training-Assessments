d59894beb95a5434edf743be0a2b86e58043df6a
Jyothi-E-N <65036042+Jyothi-E-N@users.noreply.github.com>
2023-09-27 19:46:32 +0530

    Create Assignment_Day_1_2.py

---
+++ Assignment_Day_1_2.py
@@ -0,0 +1,22 @@
+# Write a program to take "n" numbers from user and find the second largest number
+total=int(input("total values?"))
+
+count=1
+max=int(input("number#1?"))
+max2=None # we don't know yet
+
+while count<total:
+    count+=1
+    value=int(input(f"number#{count}? "))
+
+    if value>max:
+        max2=max
+        max=value
+
+    elif value<max and (max2==None or value>max2) :
+        max2=value
+
+if max!=max2:
+    print(f'second highest is {max2}')
+else:
+    print(f'All values are same. No second hightest')
